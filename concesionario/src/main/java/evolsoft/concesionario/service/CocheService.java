2eb9dedf92a2d54962816542ecc3dafc411211c8
4pablo124 <4pablo125@gmail.com>
2019-05-09 15:57:28 +0200

    Añadida ventas de coches

--- concesionario/src/main/java/evolsoft/concesionario/service/CocheService.java
+++ concesionario/src/main/java/evolsoft/concesionario/service/CocheService.java
@@ -29,5 +29,7 @@ public interface CocheService {
 	public List<CocheDTO> findCochesInPriceRange(Integer minPrice, Integer maxPrice);
 
 	public List<CocheDTO> findCochesInStock();
+
+	void newSell(Integer idCoche, Integer idCliente, Integer idVendedor) throws NotFoundExcept;
 	
 }
