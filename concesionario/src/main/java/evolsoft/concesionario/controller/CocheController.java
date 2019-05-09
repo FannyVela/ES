a109272a3145d78dd2f6081a963204c60e080bc7
Fanny Chunyan Vela Díaz <fanny.veladiaz@alum.uca.es>
2019-05-09 16:52:17 +0200

    Actualizadas etiquetas del controlador del coche

--- concesionario/src/main/java/evolsoft/concesionario/controller/CocheController.java
+++ concesionario/src/main/java/evolsoft/concesionario/controller/CocheController.java
@@ -49,21 +49,21 @@ public class CocheController {
 		cocheService.delete(id);
 	}
 
-	@RequestMapping(value = "/sortedByPrice", method = RequestMethod.GET)
+	@GetMapping("/sortedByPrice")
 	public List<CocheDTO> listCochesSortedByPrice(@RequestParam(required = false) Integer page,
-			@RequestParam(required = false) Integer size) {
-		return cocheService.listCochesSortedByPrice(page, size);
+	@RequestParam(required = false) Integer size) {
+	return cocheService.listCochesSortedByPrice(page, size);
 	}
 
-	@RequestMapping(value = "/sold", method = RequestMethod.GET)
+	@GetMapping("/sold")
 	public List<CocheDTO> findCarsAlreadySold() {
-		return cocheService.findCarsAlreadySold();
+	return cocheService.findCarsAlreadySold();
 	}
 	
-	@RequestMapping(value = "/{id}/sellCar", method = RequestMethod.GET)
-	public void sellCar(@PathVariable("id") Integer idCoche, @RequestParam(required = true) Integer idCliente,
-			@RequestParam(required = true) Integer idVendedor) throws NotFoundExcept {
-		cocheService.newSell(idCoche, idCliente, idVendedor);
+	@PutMapping("/sellCar")
+	public void sellCar(@RequestBody SoldCarDTO soldCarDTO) throws NotFoundExcept {
+	cocheService.newSell(soldCarDTO.getIdCoche(), soldCarDTO.getIdCliente(),
+	soldCarDTO.getIdVendedor());
 	}
 
 	@RequestMapping(value = "/inRange", method = RequestMethod.GET)
